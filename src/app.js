2d3f0585ff4d04c63a829b6f5b50621fe0873abb
Pranjal <pranjal.ag.1999@gmail.com>
2020-10-15 10:42:45 +0530

    Visualizer created

---
+++ src/app.js
@@ -0,0 +1,85 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.css";
+
+class Sudoku extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    getSudokuAnimations = (grid) => {
+        let zeros = 0;
+        for (let i=0; i<9; i++) {
+            for (let j=0; j<9; j++) {
+                if (grid[i][j] == 0)
+                    zeros ++;
+            }
+        }
+
+        let animations = [];
+        this.sudokuHelper(grid, 0, 0, zeros, animations);
+        
+        return animations;
+    }
+
+    sudokuHelper = (grid, row, col, zeros, animations) => {
+        if (zeros === 0) 
+            return true;
+
+        if (row == 9)
+            return false;
+
+        if (col == 9)
+            return this.sudokuHelper(grid, row+1, 0, zeros, animations);
+
+        if (grid[row][col] == 0) {
+            for (let i=1; i<=9; i++) {
+                if (this.isValid(grid, row, col, i)) {
+                    grid[row][col] = i;
+                    animations.push([row, col, i, true]);
+
+                    if (this.sudokuHelper(grid, row, col+1, zeros-1, animations))
+                        return true;
+
+                    grid[row][col] = 0;
+                    animations.push([row, col, 0, false]);
+                }
+            }
+        } else {
+            if (this.sudokuHelper(grid, row, col+1, zeros, animations))
+                return true;
+        }
+
+        return false;
+    }
+
+    isValid = (grid, row, col, value) => {
+        // check validity in row
+        for (let i=0; i<9; i++) {
+            if (grid[row][i] === value)
+                return false;
+        }
+
+        // check validity in col
+        for (let i=0; i<9; i++) {
+            if (grid[i][col] === value)
+                return false;
+        }
+
+        // check validity in sub-grid
+        let subGrid = [[0,1,2], [3,4,5], [6,7,8]];
+        let r = Math.floor(row/3);
+        let c = Math.floor(col/3);
+
+        for (let i of subGrid[r]) {
+            for (let j of subGrid[c]) {
+                if (grid[i][j] == value)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+export { Sudoku };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Sudoku } from "./app.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class SudokuVisualizer extends Sudoku {
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = {
+            grid:[
+                [3, 0, 6, 5, 0, 8, 4, 0, 0], 
+                [5, 2, 0, 0, 0, 0, 0, 0, 0], 
+                [0, 8, 7, 0, 0, 0, 0, 3, 1], 
+                [0, 0, 3, 0, 1, 0, 0, 8, 0], 
+                [9, 0, 0, 8, 6, 3, 0, 0, 5], 
+                [0, 5, 0, 0, 9, 0, 6, 0, 0], 
+                [1, 3, 0, 0, 0, 0, 2, 5, 0], 
+                [0, 0, 0, 0, 0, 0, 0, 7, 4], 
+                [0, 0, 5, 2, 0, 6, 3, 0, 0]
+            ]
+        };
+
+        // in ms
+        this.ANIMATION_SPEED = 10;
+    }
+
+    showAnimation = () => {
+        let animations = this.getSudokuAnimations(this.state.grid.slice());
+        let grid = document.getElementsByClassName("cell");
+
+        for (let i=0; i<animations.length; i++) {
+            setTimeout(() => {
+                let [row, col, value, status] = animations[i];
+                grid[row*9+col].firstChild.innerHTML = value;
+                if (status) {
+                    grid[row*9+col].firstChild.style.color = "white";
+                    grid[row*9+col].style.backgroundColor = "green";
+                }
+                else {
+                    grid[row*9+col].firstChild.style.color = "red";
+                    grid[row*9+col].style.backgroundColor = "red";
+                }
+            }, this.ANIMATION_SPEED*i);
+        }
+    }
+
+    render() {
+        let arr = this.state.grid.map((row) => {
+            return row.map((e) => {
+                return (
+                    <div className="cell">
+                        {
+                            e == 0 ? <span id="cell-element" style={{color: "blue"}}>hah</span> : <span id="cell-element">{e}</span>
+                        }
+                    </div>
+                );
+            });
+        });
+
+        let showGrid = [];
+        for (let i=0; i<9; i++) {
+            showGrid.push(arr[i]);
+            showGrid.push(<br/>);
+        }
+
+        return (
+        <div>
+            {showGrid}
+            <button type="button" onClick={this.showAnimation}>Solve</button>
+        </div>
+        );
+    }
+}
+
+let e = <SudokuVisualizer></SudokuVisualizer>;
+ReactDOM.render(e, document.getElementById("root"));
